09b70269330db3734ec460d70b5a5783940f3fd2
Krisz <krisz@example.com>
2020-06-23 12:08:51 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const port = 3000;
+const app = express();
+
+app.use(express.json());
+
+app.listen(port, () => console.log(`App is running on port: ${port}`));
